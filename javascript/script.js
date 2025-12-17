c529598416087872797285ed3c810dab10654cbb
igorledo <igorledo86@gmail.com>
2025-12-17 07:49:47 -0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,27 @@
+let pilha = [];
 
+function atualizarTela() {
+  const div = document.getElementById("pilha");
+  div.innerHTML = "";
+
+  pilha.forEach(item => {
+    const elemento = document.createElement("div");
+    elemento.className = "item";
+    elemento.innerText = item;
+    div.appendChild(elemento);
+  });
+}
+
+function adicionar() {
+  const valor = document.getElementById("valor").value;
+  if (valor !== "") {
+    pilha.push(valor);
+    document.getElementById("valor").value = "";
+    atualizarTela();
+  }
+}
+
+function remover() {
+  pilha.pop();
+  atualizarTela();
+}
